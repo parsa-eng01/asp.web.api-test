31f24227ec2682192fc6782b9d892e32706cb48b
PaRsA <parsa.eng01@gmail.com>
2024-02-19 12:56:52 +0330

    update [infrastructure]: initialize database

---
+++ TestProject.Web/Infrastructure/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestProject.Web.Core;
+
+namespace TestProject.Web.Infrastructure;
+
+public class UserContext : DbContext
+{
+  public UserContext(DbContextOptions<UserContext> options) : base(options)
+  {
+
+  }
+
+  public DbSet<User> Users { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+    var user = modelBuilder.Entity<User>();
+    user.HasKey(m => m.Id);
+    user.Property(m => m.Name)
+      .HasMaxLength(200);
+  }
+}
---
+++ TestProject.Web/Infrastructure/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestProject.Web.Core;
+
+namespace TestProject.Web.Infrastructure;
+
+public class UserRepository : IUserRepository
+{
+  private readonly UserContext _context;
+
+  public UserRepository(UserContext context)
+  {
+    _context = context;
+  }
+  public async Task CreateAsync(string name, string email, string password)
+  {
+    var user = new User(name, email, password); // no hashing because it's not purpose of test 
+    _context.Add(user);
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task<User?> LoginAsync(string email, string password)
+  {
+    var user = await _context.Users
+      .SingleOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+    return user;
+  }
+}
