e3952f000830a54007b461f683fe03d4daeaaf22
PaRsA <parsa.eng01@gmail.com>
2024-02-19 12:58:19 +0330

    update [web]: prepare web app by inject dependencies

--- TestProject.Web/Program.cs
+++ TestProject.Web/Program.cs
@@ -1,19 +1,36 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TestProject.Web;
+using TestProject.Web.Application;
 using TestProject.Web.Components;
+using TestProject.Web.Core;
+using TestProject.Web.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddDbContext<UserContext>(opt =>
+{
+  var connection = builder.Configuration.GetConnectionString("AppDb");
+  opt.UseMySql(connection, ServerVersion.AutoDetect(connection));
+});
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddMediatR(opt =>
+{
+  opt.RegisterServicesFromAssemblyContaining<Program>();
+});
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UserValidation<,>));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Error", createScopeForErrors: true);
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -23,5 +40,6 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.AddUserEndpoints();
 
 app.Run();
